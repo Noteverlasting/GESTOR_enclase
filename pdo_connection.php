dabf9dd29313fab5a1a90cc67fe5ef798f3de522
Noteverlasting <ofballester@gmail.com>
2025-05-14 14:03:46 +0200

    subidita

---
+++ pdo_connection.php
@@ -0,0 +1,31 @@
+<?php
+
+// Definimos los valores de la conexion
+
+$serverName = "localhost";
+$database = "tareas_omar";
+$port = 3307;
+$user = "root"; // este user no se debe utilizar, es mejor definir un usuario que no sea el raiz.
+$password = "CIEF1234";
+$charset = 'utf8mb4';
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $conn = new PDO ("mysql:host=$serverName;port=$port;dbname=$database", $user, $password, $options);
+    // echo "¡Conectado a $database! <br>";
+
+    foreach ($conn -> query('SELECT * FROM tareas') as $fila) {
+        // print_r($fila);
+        // echo $fila['titulo'];
+        // echo "<br>";
+
+    }
+
+} catch (PDOException $error){
+    //SI FALLA, NOS INDICARÁ EL ERROR
+    echo "Error $error";
+}
